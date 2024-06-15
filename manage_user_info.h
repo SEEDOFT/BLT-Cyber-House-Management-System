7649d3b64e7db8b47abea095a413f553f8fd58a8
DrRaspec <122063000+DrRaspec@users.noreply.github.com>
2024-06-15 19:07:45 +0700

    Add files via upload

---
+++ manage_user_info.h
@@ -0,0 +1,19 @@
+#ifndef __MANAGE_USER_INFO_H__
+#define __MANAGE_USER_INFO_H__
+
+#include "hinsyoopv2"
+
+using namespace HinsyOOP;
+
+class Design{
+	private:
+		static const int x = 12;
+        static const int y = 5;
+	public:
+		static void manage_user_info();
+		static void design(int a);
+		static void tp(int colorCode, int width, int height);
+		static void select_func(int n);
+};
+
+#endif
